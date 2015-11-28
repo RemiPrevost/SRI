06038630a69e6551578f304bc0293f1e573995d6
Remi Prevost <rprevost@etud.insa-toulouse.fr>
2015-11-28 17:18:04 +0100

    parse all HTML file into json

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+import com.google.gson.Gson;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import javax.swing.text.Document;
 import java.io.File;
 import java.io.IOException;
@@ -11,54 +13,82 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        File html = new File("data/CORPUS/D1.html");
-        org.jsoup.nodes.Document doc = Jsoup.parse(html,"UTF-8","");
-        List<Elements> balises = new ArrayList<>();
-        balises.add(doc.getElementsByTag("p"));
-        balises.add(doc.getElementsByTag("a"));
-        balises.add(doc.getElementsByTag("h1"));
-        balises.add(doc.getElementsByTag("h2"));
-        balises.add(doc.getElementsByTag("h3"));
-        balises.add(doc.getElementsByTag("h4"));
-        balises.add(doc.getElementsByTag("h5"));
-        balises.add(doc.getElementsByTag("h6"));
-        balises.add(doc.getElementsByTag("title"));
+    private static File[] findHtmls(String folder_name) {
+        File folder = new File(folder_name);
+        File[] files = folder.listFiles();
+        return files;
+    }
 
+    public static void main(String[] args) throws IOException {
+        File[] files = findHtmls("data/CORPUS/");
         File bad_words_file = new File("data/bad_words");
+
         List<String> bad_words = new ArrayList<>();
+        List<Elements> balises = new ArrayList<>();
+        List<String> inner_elements = new ArrayList<>();
+        List<String> tokens = new ArrayList<>();
+
+        String[] tab_words;
+        String json;
+        boolean found;
+        Integer i;
+        int index = 1;
+
+        Gson gson = new Gson();
+
         Scanner scanner = new Scanner(bad_words_file);
         while (scanner.hasNextLine()) {
             bad_words.add(scanner.nextLine());
         }
 
-        //String[] list = chaine.split("[.,;:?!' -/]+|");
-        List<String> inner_elements = new ArrayList<>();
-        for(Elements elements: balises) {
-            for (Element element: elements) {
-                inner_elements.add(element.text());
-            }
-        }
+        for (File file: files) {
+            org.jsoup.nodes.Document doc = Jsoup.parse(file,"UTF-8","");
 
-        List<String> tokens = new ArrayList<>();
-        String[] tab_words;
-        boolean found;
-        for (String several_words: inner_elements) {
-            tab_words = several_words.split("[.,;:?!'«» -/[\\xA0]]+");
-            for (String word: tab_words) {
-                if (word.length() > 2 && word.length() < 8) {
-                    found = false;
-                    for (String bad_word: bad_words) {
-                        if (bad_word.equals(word)) {
-                            found = true;
-                            break;
+            balises.add(doc.getElementsByTag("p"));
+            balises.add(doc.getElementsByTag("a"));
+            balises.add(doc.getElementsByTag("h1"));
+            balises.add(doc.getElementsByTag("h2"));
+            balises.add(doc.getElementsByTag("h3"));
+            balises.add(doc.getElementsByTag("h4"));
+            balises.add(doc.getElementsByTag("h5"));
+            balises.add(doc.getElementsByTag("h6"));
+            balises.add(doc.getElementsByTag("title"));
+
+            for(Elements elements: balises) {
+                for (Element element: elements) {
+                    inner_elements.add(element.text());
+                }
+            }
+
+
+            for (String several_words: inner_elements) {
+                tab_words = several_words.split("[.,;:?!\\'\\[\\]«»<>= -/[\\xA0][\\x5C]]+");
+                for (String word: tab_words) {
+                    try {
+                        i = Integer.parseInt(word);
+                    } catch (NumberFormatException e) {
+                        if (word.length() > 2 && word.length() < 8) {
+                            found = false;
+                            for (String bad_word: bad_words) {
+                                if (bad_word.equals(word)) {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            if (!found) {
+                                tokens.add(word);
+                            }
                         }
-                    }
-                    if (!found) {
-                        tokens.add(word);
                     }
                 }
             }
+            json = gson.toJson(tokens);
+            System.out.println(file.getName()+" "+json);
+            index++;
+
+            balises.clear();
+            tokens.clear();
+            inner_elements.clear();
         }
     }
 }
