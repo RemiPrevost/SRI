a912352eaa5ad7b0d56e6fa6a8067e74f599150c
Remi Prevost <rprevost@etud.insa-toulouse.fr>
2015-12-10 16:11:07 +0100

    tf and idf

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 import com.mongodb.*;
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.jsoup.select.Evaluator;
 
 import java.io.File;
 import java.io.IOException;
@@ -41,16 +43,21 @@ public class Main {
         String file_name;
         String key1 = null;
         String key2;
+        String key3;
+        String temp = "";
         boolean found;
         int index = 1;
         int word_count_all;
         int doc_count;
         int total_ite;
+        int nb_doc=1;
 
         JSONObject json1;
         JSONObject json2;
+        JSONObject json3;
         Iterator<String> iterator1;
         Iterator<String> iterator2;
+        Iterator<String> iterator3;
 
         Scanner scanner = new Scanner(bad_words_file);
         while (scanner.hasNextLine()) {
@@ -89,7 +96,7 @@ public class Main {
                     try {
                         Integer i = Integer.parseInt(word);
                     } catch (NumberFormatException e) {
-                        if (word.length() > 2 && word.length() < 8) {
+                        if (word.length() >= 2 && word.length() < 8) {
                             found = false;
                             for (String bad_word: bad_words) {
                                 if (bad_word.equals(word)) {
@@ -110,16 +117,22 @@ public class Main {
                                         try {
                                             if (cursor2.hasNext()) {
                                                 // increment
-                                                query1 = new BasicDBObject(word, new BasicDBObject("$exists",true));
+                                                query1 = new BasicDBObject(word+'.'+file_name, new BasicDBObject("$exists",true));
                                                 query2 = new BasicDBObject("$inc",
-                                                        new BasicDBObject(word+'.'+file_name,1));
+                                                        new BasicDBObject(word+"."+file_name+".it",1));
                                                 coll_token.update(query1,query2);
                                             }
                                             else {
                                                 // insert doc in exiting token
+                                                if (word.equals("abonner")) {
+                                                    System.out.println("");
+                                                }
                                                 query1 = new BasicDBObject(word, new BasicDBObject("$exists",true));
                                                 query2 = new BasicDBObject("$set",
-                                                        new BasicDBObject(word+'.'+file_name,1));
+                                                        new BasicDBObject(word+'.'+file_name,
+                                                                new BasicDBObject("it",1)
+                                                        )
+                                                );
                                                 coll_token.update(query1,query2);
                                             }
                                         }finally {
@@ -127,7 +140,10 @@ public class Main {
                                         }
                                     } else {
                                         query1 = new BasicDBObject(word,
-                                                new BasicDBObject(file_name,1));
+                                                new BasicDBObject(file_name,
+                                                        new BasicDBObject("it",1)
+                                                )
+                                        );
                                         coll_token.insert(query1);
                                     }
                                 }finally {
@@ -145,6 +161,9 @@ public class Main {
             balises.clear();
             tokens.clear();
             inner_elements.clear();
+
+            System.out.println("doc_num: "+nb_doc);
+            nb_doc++;
         }
 
         cursor1 = coll_token.find();
@@ -156,27 +175,56 @@ public class Main {
                 iterator1 = json1.keys();
                 while(iterator1.hasNext()) {
                     key1 = iterator1.next();
-                    if (!key1.equals("_id") && !key1.equals("nb_doc") && !key1.equals("total_ite")) {
+                    if (!key1.equals("_id") && !key1.equals("nb_doc") && !key1.equals("total_ite") && !key1.equals("idf")) {
+                        temp = key1;
                         json2 = json1.getJSONObject(key1);
                         iterator2 = json2.keys();
                         while(iterator2.hasNext()) {
                             key2 = iterator2.next();
                             doc_count++;
-                            total_ite+=json2.getInt(key2);
+                            json3 = json2.getJSONObject(key2);
+                            iterator3 = json3.keys();
+                            while (iterator3.hasNext()) {
+                                key3 = iterator3.next();
+                                if (key3.equals("it")) {
+                                    total_ite+=json3.getInt(key3);
+                                    query1 = new BasicDBObject("name",key2);
+                                    cursor2 = coll_doc.find(query1);
+                                    if (cursor2.hasNext()) {
+                                        String doc = cursor2.next().toString();
+                                        JSONObject doc_json = new JSONObject(doc);
+                                        word_count_all = doc_json.getInt("word_count_all");
+                                        System.out.println("doc: "+key2+" word_count: "+word_count_all);
+                                        query1 = new BasicDBObject(key1,new BasicDBObject("$exists",true));
+                                        query2 = new BasicDBObject("$set",
+                                                new BasicDBObject(
+                                                        key1+"."+key2+".tf",
+                                                        json3.getInt(key3)/(1+ Math.log10(word_count_all))
+                                                )
+                                        );
+                                        coll_token.update(query1,query2);
+                                    }
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
-                if(key1 != null) {
-                    query1 = new BasicDBObject(key1, new BasicDBObject("$exists",true));
+                if(!temp.equals("")) {
+                    System.out.println(temp);
+                    query1 = new BasicDBObject(temp, new BasicDBObject("$exists",true));
                     query2 = new BasicDBObject("$set",
                             new BasicDBObject("nb_doc",doc_count));
                     coll_token.update(query1,query2);
-                }
 
-                if(key1 != null) {
-                    query1 = new BasicDBObject(key1, new BasicDBObject("$exists",true));
+                    query1 = new BasicDBObject(temp, new BasicDBObject("$exists",true));
                     query2 = new BasicDBObject("$set",
                             new BasicDBObject("total_ite",total_ite));
+                    coll_token.update(query1,query2);
+
+                    query1 = new BasicDBObject(temp, new BasicDBObject("$exists",true));
+                    query2 = new BasicDBObject("$set",
+                            new BasicDBObject("idf",Math.log10(total_ite/doc_count)));
                     coll_token.update(query1,query2);
                 }
             }
